f282a54467a8e02208bb91f7d061b974cf7b135d
SOVAynia <allregey@gmail.com>
2026-04-06 11:23:16 +0300

    Update ConsoleApplication14.cpp

--- ConsoleApplication14/ConsoleApplication14.cpp
+++ ConsoleApplication14/ConsoleApplication14.cpp
@@ -1,32 +1,32 @@
 ﻿#include <iostream> // используемые библиотеки
-#include <cmath>
+#include <cmath>    // Использовал библиотеку cmath
 
 using namespace std;
 
 int main() {
-    double x, y;
-    cout << "Введите x:";
-    cin >> x;
-    cout << "Введите y:";        // ввод x и y
-    cin >> y;
+    double x, y;  // Числа с плавующей запятой x и y
+    cout << "Введите x:";  // Ввод числа
+    cin >> x;              // Чтение числа
+    cout << "Введите y:";  // ввод числа
+    cin >> y;              // Чтение числа 
 
-    bool inside_circle = (x * x + y * y) < 100;
-    bool on_circle = (x * x + y * y) == 100;
-                                                        // проверка условий
-    bool above_graph = (y > abs(x));
-    bool on_graph = (y == abs(x));
+    bool inside_circle = (x * x + y * y) < 100;    // Подсчёт по формуле (Логическое действие)
+    bool on_circle = (x * x + y * y) == 100;       // Подсчёт по формуле (Логическое действие)
+                                                
+    bool above_graph = (y > abs(x));               // Подсчёт по формуле (Логическое действие)
+    bool on_graph = (y == abs(x));                 // Подсчёт по формуле (Логическое действие)
 
-    if (inside_circle && above_graph)
+    if (inside_circle && above_graph)              // Проверка условий
     {
-        cout << "Ответ: да";
+        cout << "Ответ: да";                       // Вывод ответа
     }
     else if ((on_circle && y >= abs(x)) || (on_graph && (x * x + y * y) <= 100)) // Вывод ответа 
     {
-        cout << "Ответ; на границе";
+        cout << "Ответ; на границе";                // Вывод ответа
     }
     else
     {
-        cout << "Ответ: нет";
+        cout << "Ответ: нет";                       //  Ввывод ответа
     }
 
     return 0;
